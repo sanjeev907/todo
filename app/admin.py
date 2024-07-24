71de658041919c624813fad51d2ec634232f3281
Sanjeev <sanjeev@storyexp.com>
2024-07-24 14:14:54 +0530

    small changes

--- app/admin.py
+++ app/admin.py
@@ -5,6 +5,9 @@ from .models import *
 class TodoAdmin(admin.ModelAdmin):
     list_display = ['name','subject','email','address']
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name','email','password','address','phone']
+    
 admin.site.register(Todo,TodoAdmin)
 admin.site.register(User)
 admin.site.register(Todo1)
