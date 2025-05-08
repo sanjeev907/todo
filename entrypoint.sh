4dc7237ec6f0c4334c158dadef32d67d0300b554
Sanjeev Kaushik <53852894+sanjeev907@users.noreply.github.com>
2025-05-08 17:37:17 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,26 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Wait for DB (optional)
-# echo "Waiting for database..."
-# sleep 10
+# # Wait for DB (optional)
+# # echo "Waiting for database..."
+# # sleep 10
 
-echo "Applying migrations..."
+# echo "Applying migrations..."
+# python manage.py migrate
+
+# echo "Collecting static files..."
+# python manage.py collectstatic --noinput
+
+# echo "Starting uWSGI..."
+# uwsgi --ini uwsgi.ini
+
+
+#!/bin/sh
+
+echo "Applying database migrations..."
 python manage.py migrate
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "Starting uWSGI..."
-uwsgi --ini uwsgi.ini
+echo "Starting server..."
+python manage.py runserver 0.0.0.0:8010
