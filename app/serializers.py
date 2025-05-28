baf00e3780a6fdef591318c0a5edc100e0debcd6
Sanjeev Kaushik <53852894+sanjeev907@users.noreply.github.com>
2025-05-28 16:38:51 +0530

    Update serializers.py

--- app/serializers.py
+++ app/serializers.py
@@ -6,3 +6,9 @@ class TodoSerializers(serializers.Serializer):
     subject = serializers.CharField(required = True, error_messages={'subject':'subject is required'})
     email = serializers.CharField(required = True, error_messages={'email':'email is required'})
     address = serializers.CharField(required = True, error_messages={'address':'address is required'})
+
+
+class TodoViewSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
